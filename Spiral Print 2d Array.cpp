cfd02a725ad79cb26e7acab2a74384acb0741e65
bhavuk-09 <56162838+bhavuk-09@users.noreply.github.com>
2022-03-17 13:47:37 +0530

    Create Spiral Print 2d Array.cpp

---
+++ Spiral Print 2d Array.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n,m;
+    cin>>n>>m;
+    int arr[n][m];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    int row_start=0, row_end=n-1, col_start=0, col_end= m-1;
+    while(row_start<=row_end && col_start<=col_end){
+        // Printing First row
+        for(int j=col_start;j<=col_end;j++){
+            cout<<arr[row_start][j]<<" ";
+        }
+        row_start++;
+        //for col start
+        for(int i=row_start;i<=row_end;i++){
+            cout<<arr[i][col_end]<<" ";
+        } 
+        col_end--;
+
+        //for row end
+        for(int j=col_end;j>=col_start;j--){
+            cout<<arr[row_end][j]<<" ";
+        }
+        row_end--;
+
+        //for col end
+        for(int i=row_end;i>=row_start;i--){
+            cout<<arr[i][col_start]<<" ";
+        }
+        col_start++;
+    }
+    
+}
