c169aff878ab0d36f907ce857d461d1572bb45cf
bhavuk-09 <56162838+bhavuk-09@users.noreply.github.com>
2022-07-21 22:22:53 +0530

    Create Majority Element.cpp
    Given an array A of N elements. Find the majority element in the array. A majority element in an array A of size N is an element that appears more than N/2 times in the array.

---
+++ Majority Element.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+     // Function to find majority element in the array
+    // a: input array
+    // size: size of input array
+    int majorityElement(int a[], int size)
+    {
+        
+        // your code here
+        int n=size/2;
+        int res=-4000;
+        map<int,int> mp;
+        if (size==1)  return a[0];
+        
+        for(int i=0;i<size;i++){
+            mp[a[i]]++;
+        }
+        for(int i=0;i<n;i++){
+            if(mp[a[i]]>n){
+                return a[i];
+            }
+        }
+        return -1;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+        int arr[n];
+        
+        for(int i = 0;i<n;i++){
+            cin >> arr[i];
+        }
+        Solution obj;
+        cout << obj.majorityElement(arr, n) << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
