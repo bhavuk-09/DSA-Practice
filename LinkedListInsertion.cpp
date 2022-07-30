52ea5551438d04d54b9562d92f048486f058c480
bhavuk-09 <56162838+bhavuk-09@users.noreply.github.com>
2022-07-30 19:23:18 +0530

    Create LinkedListInsertion.cpp

---
+++ LinkedListInsertion.cpp
@@ -0,0 +1,99 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+void printList(Node* node) 
+{ 
+    while (node != NULL) { 
+        cout << node->data <<" "; 
+        node = node->next; 
+  }  
+  cout<<"\n";
+} 
+
+
+
+// } Driver Code Ends
+/*Structure of the linked list node is as
+struct Node {
+  int data;
+  struct Node * next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+}; */
+
+class Solution{
+  public:
+    //Function to insert a node at the beginning of the linked list.
+    Node *insertAtBegining(Node *head, int x) {
+       // Your code here
+       Node* nnode;
+       nnode= new Node(x);
+       nnode->next=head;
+       head=nnode;
+       return head;
+    }
+    
+    
+    //Function to insert a node at the end of the linked list.
+    Node *insertAtEnd(Node *head, int x)  {
+       // Your code here
+       Node* nnode;
+       nnode= new Node(x);
+       nnode->next=NULL;
+       Node*cur=head;
+       if(head ==NULL){
+           head=nnode;
+           return head;
+       }
+       
+       while(cur->next != NULL){
+           cur=cur->next;
+       }
+       cur->next= nnode;
+       return head;
+       
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        struct Node *head = NULL;
+        for (int i = 0; i < n; ++i)
+        {
+            int data, indicator;
+            cin>>data;
+            cin>>indicator;
+            Solution obj;
+            if(indicator)
+                head = obj.insertAtEnd(head, data); 
+            else
+                head = obj.insertAtBegining(head, data);
+        }
+        printList(head); 
+    }
+    return 0; 
+} 
+
+
+// } Driver Code Ends
