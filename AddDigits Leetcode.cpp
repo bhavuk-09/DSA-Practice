9aad04b4aa8fb8890f23e75a33bc4851bfa10937
bhavuk-09 <56162838+bhavuk-09@users.noreply.github.com>
2022-08-24 13:38:22 +0530

    Create AddDigits Leetcode.cpp

---
+++ AddDigits Leetcode.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        while(num>=10){
+            int sum=0;
+            int temp=num;
+            while(temp!=0){
+                sum+=temp%10;
+                temp=temp/10;
+            }
+            num=sum;
+            //cout<<sum<<";";
+        }
+        return num;
+    }
+};
