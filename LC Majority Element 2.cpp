48201243599e916652b3240bdff3fa7bb83ec58a
Bhavuk Khanna <56162838+bhavuk-09@users.noreply.github.com>
2022-08-28 00:37:49 +0530

    Update LC Majority Element 2.cpp
    Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times.

--- LC Majority Element 2.cpp
+++ LC Majority Element 2.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
-    int majorityElement(vector<int>& nums) {
+    vector<int> majorityElement(vector<int>& nums) {
+         vector<int> res;
         map<int,int> mp;
         int n=nums.size();
         for(int i=0;i<nums.size();i++){
             mp[nums[i]]++;
-            if(mp[nums[i]]> n/2) return nums[i];
         }
-        return 1;
+        for(int i=0;i<n;i++){
+            if(mp[nums[i]] > n/3){
+                res.push_back(nums[i]);
+                mp[nums[i]]=0;
+            }
+        }
+        return res;
     }
 };
