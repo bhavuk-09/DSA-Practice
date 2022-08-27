5b3c10420315c338d0f17ecad72aa0528690066f
bhavuk-09 <56162838+bhavuk-09@users.noreply.github.com>
2022-08-27 21:24:54 +0530

    Create LC Middle of the Linked List.cpp
    Given the head of a singly linked list, return the middle node of the linked list.
    
    If there are two middle nodes, return the second middle node.

---
+++ LC Middle of the Linked List.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        int count=1;
+        ListNode* temp=head;
+        
+        while(temp->next!=NULL){
+            count++;
+            temp=temp->next;
+        }
+        int mid=count/2;
+        
+        ListNode*t1=head;
+        while(mid--){
+            t1=t1->next;
+        }
+        return t1;
+    }
+};
