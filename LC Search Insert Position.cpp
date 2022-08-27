e20f3db404f3ca5d7e83fc9027308052b29bcb3c
Bhavuk Khanna <56162838+bhavuk-09@users.noreply.github.com>
2022-08-28 00:53:21 +0530

    Create LC Search Insert Position.cpp
    Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
    
    You must write an algorithm with O(log n) runtime complexity.

---
+++ LC Search Insert Position.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l=0,r=nums.size()-1;
+        // if(target>nums[nums.size()-1]) return nums.size();
+        // if(target<nums[0]) return 0;
+        int mid=0;
+        while(l<=r){
+            mid= (l+r)/2;
+            if(nums[mid]== target){
+                return mid;
+            }
+            else if( nums[mid]<target){
+                l=mid+1;
+            }
+            else r=mid-1;
+        }
+        return l;
+    }
+};
