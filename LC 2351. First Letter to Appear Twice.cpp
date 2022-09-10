168556375a0dc5e7741b7be23cb83dd7c1b74dac
Bhavuk Khanna <56162838+bhavuk-09@users.noreply.github.com>
2022-09-10 23:22:52 +0530

    Create LC 2351. First Letter to Appear Twice.cpp

---
+++ LC 2351. First Letter to Appear Twice.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    char repeatedCharacter(string s) {
+        set<char> st;
+        for(int i=0;i<s.length();i++){
+            if(st.find(s[i]) != st.end()){
+                return s[i];
+            }
+            else st.insert(s[i]);
+        }
+        return '1';
+    }
+};
