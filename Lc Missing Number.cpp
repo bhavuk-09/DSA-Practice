e39ab04aed69ca018f65f2c8428d944112e24e92
Bhavuk Khanna <56162838+bhavuk-09@users.noreply.github.com>
2022-09-16 12:48:10 +0530

    O(n) Time; O(1) Space

---
+++ Lc Missing Number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+        }
+        int n=nums.size();
+        int s=(n* (n+1))/2;
+        return s-sum;
+    }
+};
