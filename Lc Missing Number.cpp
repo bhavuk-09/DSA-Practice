74c1c8795e05c1bd9e1391f850585e7754523228
Bhavuk Khanna <56162838+bhavuk-09@users.noreply.github.com>
2022-09-16 12:49:12 +0530

    Update Lc Missing Number.cpp
    Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.

--- Lc Missing Number.cpp
+++ Lc Missing Number.cpp
@@ -11,3 +11,4 @@ public:
         return s-sum;
     }
 };
+// Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
